a1d8841e00f7e957fbc1b93973c23f412b881f68
Marwa Sh <marwa.al.shahal@gmail.com>
2021-03-18 19:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,43 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection,callB) { 
+      let coll= (Array.isArray(collection))? collection.slice(): Object.values(collection)
+      for (let index=0; index<coll.length; index++)
+      callB(coll[index])
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection,callB) {
+      if (!(Array.isArray(collection)))
+         collection=Object.values(collection)
+      const newarray=[];
+      
+      for (let i=0; i<collection.length; i++)
+      newarray.push(callB(collection[i]));
+      return newarray;
+    
     },
 
-    reduce: function() {
-
+    reduce: function(arr=[], callB = () => {}, acc) {
+        if(!acc)
+        acc = collection[0]
+        
+        for (let elt of arr){
+          acc= CallB(acc,elt)
+        }
+        return acc;
     },
 
-    functions: function() {
-
+    filter: function(array, test) {
+         let passed = [];
+         for (let element of array) {
+         if (test(element)) {
+         passed.push(element);
+           }
+       }
+     return passed;
+    
     },
 
 
